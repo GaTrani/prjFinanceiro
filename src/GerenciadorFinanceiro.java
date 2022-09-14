f4409da1d0d7d2e1e8620f9e05da1d7ef89055d5
GaTrani <gabrieltrani02@gmail.com>
2022-09-13 22:24:54 -0300

    up

--- src/GerenciadorFinanceiro.java
+++ src/GerenciadorFinanceiro.java
@@ -1,5 +1,16 @@
+import com.xyz.financeiro.view.ContaView;
+
+import javax.swing.*;
+
 public class GerenciadorFinanceiro {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        JFrame tela = new JFrame();
+        tela.setSize(500,500);
+        tela.setTitle("Controle de Contas");
+        tela.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        tela.setLocationRelativeTo(null);
+        tela.setContentPane(new ContaView().getPainelConta());
+
+        tela.setVisible(true);
     }
 }
