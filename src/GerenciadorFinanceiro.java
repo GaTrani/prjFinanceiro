a14689c2d62d7a84e71d1b8f98605d6168a04987
GaTrani <gabrieltrani02@gmail.com>
2022-09-13 21:51:59 -0300

    up

---
+++ src/GerenciadorFinanceiro.java
@@ -0,0 +1,5 @@
+public class GerenciadorFinanceiro {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/com/xyz/financeiro/view/ContaView.java
@@ -0,0 +1,17 @@
+package com.xyz.financeiro.view;
+
+import javax.swing.*;
+
+public class ContaView {
+    private JPanel painelCliente;
+    private JTextField textNumConta;
+    private JTextField textNomeCliente;
+    private JButton depositarButton;
+    private JButton sacarButton;
+    private JTextField textValorDeposito;
+    private JTextField textValorSaque;
+    private JButton cadastrarContaButton;
+    private JButton FECHARButton;
+    private JLabel labelSaldo;
+    private JLabel labelMsg;
+}
